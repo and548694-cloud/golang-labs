d247db2c92b8071654b0c88f487aee6f5d0f266e
and548694-cloud <and548694@gmail.com>
2026-03-21 11:08:07 +0200

    lab1

---
+++ lab1/second.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func second() {
+	var y float64
+	x := rand.Float64()*70 - 10
+
+	switch {
+	case x >= 1 && x <= 50:
+		if x*x-4 == 0 {
+			fmt.Println("Помилка: ділення на нуль")
+			return
+		}
+		y = x / (x*x - 4)
+	default:
+		y = 2 * x * x
+	}
+
+	fmt.Printf("x: %.2f\n", x)
+	fmt.Printf("Result: %.4f\n", y)
+}
